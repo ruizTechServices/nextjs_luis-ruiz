64e9668be4b5737bba9988fda07ca6a2ea16ddbe
Luis Ruiz <ruiztechservices@gmail.com>
2024-04-01 01:13:03 -0400

    designed dashboard much better

--- app/components/main/journalEntry.js
+++ app/components/main/journalEntry.js
@@ -1,19 +1,27 @@
 // C:\Users\NEWOWNER\local_only\local_ruiztechservices\nextjs_luis-ruiz\app\components\main\journalEntry.js
 //Technically this is the card component that displays the journal entry
-import Link from "next/link";
-// import { useEffect, useState } from "react";
-// import supabase from "../../utils/supabase/supabaseClient";
 
-export const JournalEntry = ({ id, title, content }) => (
-  <>
-    <div className="w-1/2 p-4 border-2 rounded-lg shadow-xl mb-5">
-      <p className="text-4xl font-bold">{title}</p>
-      <p className="italic">{content}</p>
-      {/* <div className="flex justify-end button">
-        <Link className="text-blue-600" href={'/' + id }>
-          View Entry
-        </Link>
-      </div> */}
-    </div>
-  </>
-);
+import React from 'react'; // Import React
+
+export const JournalEntry = ({ id, title, content }) => {
+    const [showMore, setShowMore] = React.useState(false);
+    const toggleShowMore = () => setShowMore(!showMore);
+    const previewContent = content.split(". ").slice(0, 2).join(". ") + ".";
+
+    return (
+        <>
+      <div className="bg-white dark:bg-gray-800 p-4 rounded-2xl shadow-md mb-5 mx-auto text-center w-full md:w-1/3">
+              <p className="text-4xl font-bold text-black dark:text-white">{title}</p>
+              <p className="italic text-gray-900 dark:text-gray-100">
+                  {showMore ? content : previewContent}
+                 <button
+                     onClick={toggleShowMore}
+                     className="text-blue-500 hover:text-blue-700 transition duration-300 ease-in-out"
+                 >
+                     {showMore ? " less" : " more"}
+                 </button>
+              </p>
+          </div>
+        </>
+    );
+};
