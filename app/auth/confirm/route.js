d4e401e3b17809389bc0be87d0dd67848dc34ab2
Luis Ruiz <ruiztechservices@gmail.com>
2024-04-02 18:57:58 -0400

    updated modules to routes

--- app/auth/confirm/route.js
+++ app/auth/confirm/route.js
@@ -3,7 +3,7 @@ import { EmailOtpType } from '@supabase/supabase-js'
 import { NextRequest, NextResponse } from 'next/server'
 
 
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '../../utils/supabase/server'
 
 export async function GET(request, NextRequest) {
   const { searchParams } = new URL(request.url);
