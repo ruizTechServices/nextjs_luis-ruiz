9d4ef28466a4279f8e64248d8dfad68b74df320a
Luis Ruiz <ruiztechservices@gmail.com>
2024-04-13 09:49:29 -0400

    updated signout functionality in app/signout/page.js

--- app/signout/page.js
+++ app/signout/page.js
@@ -1,22 +1,35 @@
+//C:\Users\NEWOWNER\local_only\local_ruiztechservices\nextjs_luis-ruiz\app\signout\page.js
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import supabase from "../../lib/utils/supabase/supabaseClient";
 
 export default function SignOut() {
   const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
+    // Mark component as being mounted in a client environment
+    setIsClient(true);
+    if (!isClient) return; // Ensure that sign-out logic only runs on the client side
+
     const signOutUser = async () => {
-      let { error } = await supabase.auth.signOut();
+      const { error } = await supabase.auth.signOut();
       if (!error) {
         router.push("/signin");
+      } else {
+        // Optionally handle the error, perhaps by displaying a message
+        console.error("Error during sign out:", error.message);
       }
-      // Handle error if needed
     };
 
     signOutUser();
-  }, [router]);
+  }, [isClient, router]);
+
+  if (!isClient) {
+    // Optionally render a loading indicator or nothing while waiting for the client environment
+    return null;
+  }
 
   return (
     <div className="flex items-center justify-center h-screen">
