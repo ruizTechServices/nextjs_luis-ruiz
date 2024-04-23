4131af16853d0c1dfa22c176ef0af2143dff607c
Luis Ruiz <ruiztechservices@gmail.com>
2024-04-23 03:53:24 -0400

    updated contact design and implemented new contact information card component

--- app/thank-you/page.js
+++ app/thank-you/page.js
@@ -1,8 +1,12 @@
 export default function ThankYouPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold">Thank you for your submission!</h1>
-      <p>I will personally will see to that I will get in contact with you as soon as possible!</p>
+      <h1 className="bg-white text-3xl font-bold m-5 border p-5 rounded-xl shadow-xl">Thank you for your submission!</h1>
+      <div className="md:w-1/2 w-3/4 p-6 bg-white rounded-lg shadow-md">
+        <p>I will personally see to it that I get in contact with you as soon as possible!</p>
+        <p>Feel free to view around my websites to my previous projects, current life status, current
+          tech projects I am working on and so much more!...</p>
+      </div>
     </div>
   );
 }
