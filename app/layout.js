9f7d705d99a55ed30bf43d2a320bcaf8452944d4
Luis Ruiz <ruiztechservices@gmail.com>
2024-04-28 02:55:43 -0400

    deleted extra adsense file and data

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import MainFooter from "./components/main/mainFooter";
 import Head from "next/head";
-import Adsense from "../app/components/main/adsense"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,22 +14,26 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <><Head>
-      <meta charSet="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <meta name="description" content="Luis Ruiz Portfolio" />
-      <meta name="author" content="<NAME>" />
-      <meta
-        name="keywords"
-        content="Luis Ruiz, Portfolio, Full-Stack Developer" />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      <link rel="manifest" href="/manifest.json" />
-      <Adsense pId="ca-pub-8779702295184066" />
-    </Head><body className={inter.className}>
+    <html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="description" content="Luis Ruiz Portfolio" />
+        <meta name="author" content="<NAME>" />
+        <meta
+          name="keywords"
+          content="Luis Ruiz, Portfolio, Full-Stack Developer"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="google-adsense-account" content="ca-pub-8779702295184066" />
+      </Head>
+      <body className={inter.className}>
         {children}
         <MainFooter />
-      </body></>
+      </body>
+    </html>
   );
 }
