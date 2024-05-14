20bcfa1271193b6c1d0d72c77d62b26e41487cad
Luis Ruiz <ruiztechservices@gmail.com>
2024-05-14 19:01:14 -0400

    simple changes

--- app/catalog/page.js
+++ app/catalog/page.js
@@ -1,22 +1,7 @@
 import React from "react";
 import CatalogMain from "../components/main/catalog/catalogMain";
 
-const products = [
-  {
-    id: 1,
-    name: "Product 1",
-    description: "Description of Product 1.",
-    imageUrl: "/images/appsImage.jpg",
-    options: ["Size", "Color", "Style"]
-  },
-  {
-    id: 2,
-    name: "Product 2",
-    description: "Description of Product 2.",
-    imageUrl: "/images/appsImage.jpg",
-    options: ["Size", "Material"]
-  }
-];
+
 
 const headerTitle = <h1 className="text-3xl font-bold mb-4">Website Templates Catalog</h1>;
 
@@ -25,7 +10,7 @@ export default function Catalog() {
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
       <div className="my-20 text-center flex flex-col container mx-auto">
       {headerTitle}
-      <CatalogMain products={products} />
+      <CatalogMain />
       </div>
       
     </main>
--- app/components/ui/footer.js
+++ app/components/ui/footer.js
@@ -9,33 +9,69 @@ import { FaLinkedin } from "react-icons/fa";
 
 function Footer(props) {
   return (
-    <footer className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-800 via-gray-400 to-gray-600 text-white p-6 flex flex-col items-center sm:flex-row sm:justify-between">
-      <div className="border-red-500 w-20">
-        <Link href={'/'}>
+    <footer className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-800 via-gray-400 to-gray-600 text-white py-6">
+      <div className="flex-col items-center md:flex-row space-y-4 md:hidden flex">
+        <div className="border-red-500 w-20">
+          <Link href={'/'}>
             <Logo />
-        </Link> 
+          </Link>
+        </div>
+        <div className="flex flex-row items-center space-x-4 sm:space-x-4">
+          <Link href={"https://www.facebook.com/giovanni.ruiz.522066"} className="hover:text-blue-500 transition-colors">
+            <i className="fab fa-facebook-f"></i> <FaFacebook />
+          </Link>
+          <Link href={"https://twitter.com/giosterr44"} className="hover:text-blue-300 transition-colors">
+            <i className="fab fa-twitter"></i><FaTwitter />
+          </Link>
+          <Link href={"https://www.instagram.com/peacefulpython44/"} className="hover:text-pink-500 transition-colors">
+            <i className="fab fa-instagram"></i> <PiInstagramLogoBold />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/ruiz44/"} className="hover:text-blue-700 transition-colors">
+            <i className="fab fa-linkedin-in"></i> <FaLinkedin />
+          </Link>
+        </div>
+        <div className="flex flex-row items-center space-x-4">
+          <Dropdown links={props.links} />
+          <AuthButton />
+        </div>
+        <div className="flex flex-col md:flex-row mb-4 sm:mb-0">
+          <div>
+            &copy;{new Date().getFullYear()} <Link href={'https://ruiztechservices.com'}>ruizTechServices</Link>
+            <span className="animate-blink">|</span>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-row items-center space-x-0 sm:space-x-4">
-        <Link href={"https://www.facebook.com/giovanni.ruiz.522066"} className="hover:text-blue-500 transition-colors">
-          <i className="fab fa-facebook-f"></i> <FaFacebook />
-        </Link>
-        <Link href={"https://twitter.com/giosterr44" }className="hover:text-blue-300 transition-colors">
-          <i className="fab fa-twitter"></i><FaTwitter />
-        </Link>
-        <Link href={"https://www.instagram.com/peacefulpython44/"} className="hover:text-pink-500 transition-colors">
-          <i className="fab fa-instagram"></i> <PiInstagramLogoBold />
-        </Link>
-        <Link href={"https://www.linkedin.com/in/ruiz44/"} className="hover:text-blue-700 transition-colors">
-          <i className="fab fa-linkedin-in"></i> <FaLinkedin />
-        </Link>
-      </div>
-      <div className="mb-4 sm:mb-0">
-        &copy;{new Date().getFullYear()} <Link href={'https://ruiztechservices.com'}>ruizTechServices</Link><span className="animate-blink">|</span> Luis-Ruiz.com All
-        rights reserved.
-      </div>
-      <div className="flex flex-row items-center space-x-4">
-        <Dropdown links={props.links} />
-        <AuthButton />
+
+      <div className="justify-evenly flex-row items-center md:flex-row space-y-4 md:flex hidden">
+        <div className="border-red-500 w-20">
+          <Link href={'/'}>
+            <Logo />
+          </Link>
+        </div>
+        <div className="flex flex-row items-center space-x-0 sm:space-x-4">
+          <Link href={"https://www.facebook.com/giovanni.ruiz.522066"} className="hover:text-blue-500 transition-colors">
+            <i className="fab fa-facebook-f"></i> <FaFacebook />
+          </Link>
+          <Link href={"https://twitter.com/giosterr44"} className="hover:text-blue-300 transition-colors">
+            <i className="fab fa-twitter"></i><FaTwitter />
+          </Link>
+          <Link href={"https://www.instagram.com/peacefulpython44/"} className="hover:text-pink-500 transition-colors">
+            <i className="fab fa-instagram"></i> <PiInstagramLogoBold />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/ruiz44/"} className="hover:text-blue-700 transition-colors">
+            <i className="fab fa-linkedin-in"></i> <FaLinkedin />
+          </Link>
+        </div>
+        <div className="flex flex-col md:flex-row mb-4 sm:mb-0">
+          <div>
+            &copy;{new Date().getFullYear()} <Link href={'https://ruiztechservices.com'}>ruizTechServices</Link>
+            <span className="animate-blink">|</span>
+          </div>
+        </div>
+        <div className="flex flex-row items-center space-x-4">
+          <Dropdown links={props.links} />
+          <AuthButton />
+        </div>
       </div>
     </footer>
   );
