bf2be0eb387fc1a8ba408d2e5a7c861db3d5b1db
Luis Ruiz <ruiztechservices@gmail.com>
2024-05-15 15:07:10 -0400

    updated iframe.js design

--- app/components/main/iframe.js
+++ app/components/main/iframe.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import Link from 'next/link';
 
 const projects = [
-  { url: 'https://catherineruiz.com', title: 'Project 1' },
-  { url: 'https://Compulogicpc.com', title: 'Project 2' },
-  // { url: 'https://ruiztechservices.com', title: 'Project 3' },
-  { url: 'https://24hourgpt.com', title: 'Project 4' },
-  // { url: 'https://demon-child.com', title: 'Project 5' },
-  { url: 'https://letmeexplain.online', title: 'Project 6' },
-  // { url: 'https://rrtruckingservices.com', title: 'Project 7' },
-  // { url: 'https://dont-download.com', title: 'Project 8' },
+  { url: "https://catherineruiz.com", title: "Catherine Ruiz's Website" },
+  { url: "https://Compulogicpc.com", title: "Compulogic" },
+  // { url: 'https://ruiztechservices.com', title: 'https://ruiztechservices.com' },
+  { url: "https://24hourgpt.com", title: "24 Hour GPT: Premium Chatbot for $1 a day" },
+  // { url: 'https://demon-child.com', title: 'https://demon-child.com' },
+  { url: 'https://letmeexplain.online', title: 'Let Me Explain: An online feedback platform' },
+  // { url: 'https://rrtruckingservices.com', title: 'https://rrtruckingservices.com' },
+  // { url: 'https://dont-download.com', title: 'https://dont-download.com' },
 ];
 
 const ProjectViewer = () => {
   return (
-    <div className="mx-auto my-auto md:max-w-[700px] w-auto">
-      <div className="flex flex-col md:flex-row md:flex-wrap justify-center py-5 space-y-4 space-x-2">
+    <div className="">
+      <div className="border-2 border-gray-600 rounded-xl p-2 container mx-auto flex flex-col items-center justify-center mb-48 flex-wrap md:flex-row">
         {projects.map((project, index) => (
-          <iframe
+          <div
             key={index}
-            src={project.url}
-            title={project.title}
-            className="aspect-video shadow-2xl rounded-xl border-2 border-gray-600"
-          />
+            className="text-center flex flex-col items-center justify-center"
+          >
+            <iframe
+              src={project.url}
+              title={project.title}
+              className="w-[400px] h-[500px] shadow-2xl rounded-xl border-2 border-gray-600"
+            />
+            <Link className='hover:cursor-pointer' href={project.url}><p>{project.title}</p></Link>
+          </div>
         ))}
       </div>
     </div>
