2ae73f1325ce4087c960c16be34e5e7bcf3a83f9
Luis Ruiz <114837126+ruizTechServices@users.noreply.github.com>
2024-10-01 14:58:57 +0000

    updated huggingface chatbot

--- pages/api/hugging_face/chat.js
+++ pages/api/hugging_face/chat.js
@@ -1,31 +1,19 @@
 //pages/api/hugging_face/chat.js
-import Together from "together-ai";
+import Together from 'together-ai';
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const together = new Together({ apiKey: process.env.TOGETHER_API_KEY });
+const together = new Together({
+  apiKey: process.env['TOGETHER_API_KEY'],
+});
 
-    try {
-      const response = await together.chat.completions.create({
-        messages: req.body.messages,
-        model: "mistralai/Mixtral-8x22B-Instruct-v0.1",
-        max_tokens: 512,
-        temperature: 0.7,
-        top_p: 0.7,
-        top_k: 50,
-        repetition_penalty: 1,
-        stop: ["</s>", "[/INST]"],
-        stream: true,
-      });
+const stream = await together.chat.completions.create({
+  model: 'meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo',
+  messages: [
+    { role: 'user', content: req.body.messages },
+  ],
+  stream: true,
+});
 
-      res.status(200).json({ content: response.choices[0].message.content });
-    } catch (error) {
-      console.error("API Error:", error);
-      res.status(500).json({ error: "An error occurred while processing your request." });
-    }
-  } else {
-    // Respond with a 405 if the method is not POST
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+for await (const chunk of stream) {
+  // use process.stdout.write instead of console.log to avoid newlines
+  process.stdout.write(chunk.choices[0]?.delta?.content || '');
 }
